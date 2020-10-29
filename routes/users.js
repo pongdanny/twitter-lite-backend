078dae7f6bb7dac1482ea0fa1d79f809028d87d1
pongdanny <pongdaniel17@gmail.com>
2020-10-29 12:38:56 -0400

    jwt setup

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,13 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const router = express.Router();
 const db = require("../db/models/index");
-const {User} = db;
-const {handleValidationErrors, asyncHandler, check} = require("./utils");
+const { User } = db;
+const { handleValidationErrors, asyncHandler, check } = require("./utils");
+const getUserToken = require("../auth");
 
-const validateUsername =
-  check("username")
-    .exists({ checkFalsy: true })
-    .withMessage("Please provide a username");
+const validateUsername = check("username")
+  .exists({ checkFalsy: true })
+  .withMessage("Please provide a username");
 
 const validateEmailAndPassword = [
   check("email")
@@ -20,11 +20,21 @@ const validateEmailAndPassword = [
     .withMessage("Please provide a password."),
 ];
 
-router.post("/", validateUsername, validateEmailAndPassword, handleValidationErrors,
-asyncHandler(async (req, res, next) => {
-    const {username, email, password} = req.body;
+router.post(
+  "/",
+  validateUsername,
+  validateEmailAndPassword,
+  handleValidationErrors,
+  asyncHandler(async (req, res, next) => {
+    const { username, email, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
-    const user = await User.create({username, email, hashedPassword})
-}));
+    const user = await User.create({ username, email, hashedPassword });
+    const token = getUserToken(user);
+    res.status(201).json({
+      user: { id: user.id },
+      token,
+    });
+  })
+);
 
 module.exports = router;
